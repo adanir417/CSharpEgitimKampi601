b91cbd74917f0abab77445dbb8544af914bdfbaf
adanir417 <adanir417@hotmail.com>
2025-01-08 01:46:04 +0300

    MongoDB-PostgreDB Completed

--- CSharpEgitimKampi601/Program.cs
+++ CSharpEgitimKampi601/Program.cs
@@ -19,6 +19,26 @@ namespace CSharpEgitimKampi601
             //Application.Run(new Form1());
             //Application.Run(new FrmCustomer());
             Application.Run(new FrmEmployee());
+
+            // Don't Forget to add App.Config OR Change App.config.example to App.config and do your connection string config.
+            /*
+          <?xml version="1.0" encoding="utf-8"?>
+          <configuration>
+            ...
+            ...
+             
+            <connectionStrings>
+                     <clear />
+                     <add name="mongoDBLearning" connectionString="mongodb://localhost:27017" />
+                     <add name="postgreDBLearning" connectionString="Server=localhost;Port=5432;Database=CustomerDb;User Id=postgres;Password=YOURPOSTGREPASSWORD;" />    
+           </connectionStrings>
+
+            ...
+            ...
+         </configuration>
+             
+             */
+
         }
     }
 }
